e85ff643327ade9fedb429907496f8dfba6bc340
emreusun <emre95neo@gmail.com>
2022-01-20 18:52:01 -0500

    uup

--- includes/submit.php
+++ includes/submit.php
@@ -1,6 +1,20 @@
 <?php
 
+if (! array_key_exists('HTTP_REFERER', $_SERVER)) {
+   die;
+}
+
+if (count($_POST) <= 0) {
+    header('Location: ' . $_SERVER['HTTP_REFERER']);
+   die;
+}
+
+var_dump(
+    $_SERVER['HTTP_REFERER']
+);
+
+ 
 var_dump($_POST);
 
-echo "Hello World";
 die;
+header('Location: ' . $_SERVER['HTTP_REFERER']);
